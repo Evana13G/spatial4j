c21e8894f15f8544b5416fc6dfaa58c4665e6ccb
Gizzi <egizzi@mm158708-pc.mitre.org>
2013-12-30 14:00:34 -0500

    tests

--- src/main/java/com/spatial4j/core/context/CirclePolygonizer.java
+++ src/main/java/com/spatial4j/core/context/CirclePolygonizer.java
@@ -19,24 +19,24 @@ public class CirclePolygonizer {
 
   public static void main(String[] args) {
     SpatialContext ctx = new SpatialContext(false, new CartesianDistCalc(), new RectangleImpl(0, 100, 200, 300, null));
-    CirclePolygonizer CirclePolygonizerTest = new CirclePolygonizer(ctx);
-    List<Point> listOfPoints = CirclePolygonizerTest.getEnclosingPolygon(0.1);
+    Circle circle = ctx.makeCircle(50.0, 250.0, 10.0);
+    CirclePolygonizer CirclePolygonizerTest = new CirclePolygonizer(ctx, circle);
+    List<Point> listOfPoints = CirclePolygonizerTest.getEnclosingPolygon(1);
     for(int i=0;i<listOfPoints.size(); i++){
       System.out.print(listOfPoints.get(i));
       System.out.print('\n');
     }
   }
 
+
   protected SpatialContext ctx;
   protected Circle circ;
 
-  public CirclePolygonizer(SpatialContext ctx){
+  public CirclePolygonizer(SpatialContext ctx, Circle circ){
     this.ctx = ctx;
-    this.circ = ctx.makeCircle(50.0, 250.0, 10.0);
+    this.circ = circ;
   }
 
-  //Note: tolerance will eventually be the maximum distance away from circle
-  // for now, we will use it as the number of iterations to use
   public List<Point> getEnclosingPolygon(double tolerance){
     Point definingPoint1 = ctx.makePoint(circ.getCenter().getX(), circ.getCenter().getY()+circ.getRadius());
     Point definingPoint2 = ctx.makePoint(circ.getCenter().getX()+circ.getRadius(), circ.getCenter().getY());
@@ -44,17 +44,11 @@ public class CirclePolygonizer {
     CartesianLine line_2 = new CartesianLineImpl (10000000.0, definingPoint2, this.ctx);
 //    CartesianLine line_2 = new CartesianLineImpl (Double.POSITIVE_INFINITY, definingPoint2, this.ctx);
     ArrayList<Point> listOfPoints = new ArrayList<Point>();
+    listOfPoints.add(definingPoint1);
     recursiveIter(tolerance, line_1, line_2, listOfPoints);
+    listOfPoints.add(definingPoint2);
     return listOfPoints;
   }
-
-//  public CartesianLine iterate(CartesianLine line_1, CartesianLine line_2){
-//    Point interPoint = calcLineIntersection(line_1, line_2);
-//    CartesianLine centerLine = new CartesianLineImpl(calcSlope(circ.getCenter(), interPoint), interPoint, ctx);
-//    Point p = calcCircleIntersection(circ, centerLine);
-//    CartesianLine tangentLine = new CartesianLineImpl(-1/calcSlope(circ.getCenter(), interPoint), p, ctx);
-//    return tangentLine;
-//  }
 
   public Point calcLineIntersection(CartesianLine L1, CartesianLine L2){
     double X = ((L1.getSlope()*L1.getDefiningPoint().getX()) - L1.getDefiningPoint().getY() -
@@ -88,24 +82,6 @@ public class CirclePolygonizer {
   public double calcSlope(Point P1, Point P2){
     return (P2.getY()-P1.getY())/(P2.getX()-P1.getX());
   }
-
-//  public List<Point> combineLists(List<Point> lst1, List<Point> lst2){
-//    List<Point> lst3 = new ArrayList<Point>();
-//    if (){
-//
-//    }else{
-//
-//    }
-//
-//    int minSize = (lst1.size()<=lst2.size() ? lst1.size() : lst2.size());
-//    for (int i=0; i<minSize; i++){
-//      if(!lst1.contains(lst2[i])){
-//        lst3.add(lst2[i]);
-//      };
-//    };
-//    lst1.addAll(lst2);
-//    return lst1;
-//  }
 
   public void recursiveIter(double tolerance, CartesianLine line1, CartesianLine line2, List<Point> listOfPoints){
     Point lineIntersectionPoint = calcLineIntersection(line1, line2);
--- src/test/java/com/spatial4j/core/context/CirclePolygonizerTest.java
+++ src/test/java/com/spatial4j/core/context/CirclePolygonizerTest.java
@@ -10,11 +10,12 @@ import com.spatial4j.core.shape.impl.CartesianLineImpl;
 import com.spatial4j.core.shape.impl.RectangleImpl;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+
 
 /**
  * Created by egizzi on 12/27/13.
- */
+*/
 public class CirclePolygonizerTest extends RandomizedShapeTest{
 
   public static final double EPS = 0.00001;
@@ -24,24 +25,46 @@ public class CirclePolygonizerTest extends RandomizedShapeTest{
 
   //constructor for test class?
   public CirclePolygonizerTest() {
-    ctx = new SpatialContext(false, new CartesianDistCalc(), new RectangleImpl(0, 100, 200, 300, null));
-    polygonizer = new CirclePolygonizer(ctx);
-    circ = ctx.makeCircle(50, 250, 10);
+    ctx = new SpatialContext(false, new CartesianDistCalc(), new RectangleImpl(0, 100, 0, 100, null));
+    circ = ctx.makeCircle(50, 50, 10);
+    polygonizer = new CirclePolygonizer(ctx, circ);
   }
 
   @Test
   public void testCalcLineIntersection(){
-    Point point1 = ctx.makePoint(0,200);
-    Point point2 = ctx.makePoint(100, 200);
+    Point point1 = ctx.makePoint(0,0);
+    Point point2 = ctx.makePoint(100, 0);
     CartesianLine line1 = new CartesianLineImpl(1, point1, ctx);
     CartesianLine line2 = new CartesianLineImpl(-1, point2, ctx);
     assertEquals(50, polygonizer.calcLineIntersection(line1, line2).getX(), EPS);
-    assertEquals(250, polygonizer.calcLineIntersection(line1, line2).getY(), EPS);
+    assertEquals(50, polygonizer.calcLineIntersection(line1, line2).getY(), EPS);
   }
-//  public void testGetEnclosingPolygon(){;}
 
-  //test for constructor ?
-  //test for both methods
+  @Test
+  public void testGetEnclosingPolygon(){
+    double tolerance = 200;
+    Point definingPoint1 = ctx.makePoint(50, 60);
+    Point definingPoint2 = ctx.makePoint(60, 50);
+    CartesianLine line_1 = new CartesianLineImpl (0.0, definingPoint1, this.ctx);
+    CartesianLine line_2 = new CartesianLineImpl (10000000.0, definingPoint2, this.ctx);
+    ArrayList<Point> listOfPoints = new ArrayList<Point>();
+
+//    listOfPoints.add(ctx.makePoint(54.142135916624156,60.0));
+//    listOfPoints.add(ctx.makePoint(57.071068165418836,57.07106745831209));
+//    listOfPoints.add(ctx.makePoint(60.00000041421349,54.142134916624215));
+
+
+    listOfPoints.add(ctx.makePoint(50.0,60.0));
+    listOfPoints.add(ctx.makePoint(60.0,60.0));
+    listOfPoints.add(ctx.makePoint(60.0,50.0));
+
+    ArrayList<Point> testListOfPoints = new ArrayList<Point>();
+    polygonizer.recursiveIter(tolerance, line_1, line_2, testListOfPoints);
+    assertEquals(listOfPoints, testListOfPoints);
+
+  }
+
+
 //  public void testCalcCircleIntersection(){;}
 //  public void testCalcTangentLine(){;}
 //  public void testCalcSlope(){;}
